c3534809b97b6f398b544f7b4f8a8f0514a84dd1
kit-o <hboesch00@gmail.com>
2025-03-28 17:52:50 -0400

    Update kit.sh

--- kit.sh
+++ kit.sh
@@ -1,8 +1,8 @@
 # Clone the repository
-git clone https://github.com/<username>/<username>.github.io.git
+git clone https://github.com/kit-o/kit-o.github.io.git
 
 # Navigate to the repository directory
-cd <username>.github.io
+cd kit-o.github.io
 
 # Add your website files (index.html, styles.css, etc.)
 echo "<!DOCTYPE html><html><head><title>My Website</title></head><body><h1>Hello, world!</h1></body></html>" > index.html
