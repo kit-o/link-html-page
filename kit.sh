59a9d2bfcd46d3b9437fe02b904b28570d5d5d44
kit-o <hboesch00@gmail.com>
2025-03-28 17:49:18 -0400

    Create kit.sh

---
+++ kit.sh
@@ -0,0 +1,15 @@
+# Clone the repository
+git clone https://github.com/<username>/<username>.github.io.git
+
+# Navigate to the repository directory
+cd <username>.github.io
+
+# Add your website files (index.html, styles.css, etc.)
+echo "<!DOCTYPE html><html><head><title>My Website</title></head><body><h1>Hello, world!</h1></body></html>" > index.html
+
+# Add and commit the changes
+git add .
+git commit -m "Initial commit"
+
+# Push the changes to GitHub
+git push origin main
